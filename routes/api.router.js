e4f84401957297d844019760bfa5dd6ef5f950c0
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-12-10 13:05:19 +0100

    add cloundinary server-side functionality. Add cloudinary keys to the env. file.

--- routes/api.router.js
+++ routes/api.router.js
@@ -6,6 +6,23 @@ const Post = require("../models/Post.model");
 const Comment = require("../models/Comment.model");
 const TravelLog = require("../models/TravelLog.model");
 
+//require uploader, already exported from cloudinary-setup.js
+const uploader = require("./../configs/cloudinary-setup");
+
+// include CLOUDINARY:
+//upload a single image per once.
+// ADD an horitzontal middleware
+router.post("/upload", uploader.single("image"), (req, res, next) => {
+    console.log("file is: ", req.file);
+
+    if (!req.file) {
+        next(new Error("No file uploaded!"));
+        return;
+    }
+    // get secure_url from the file object and save it in the
+    // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+    res.json({ secure_url: req.file.secure_url });
+});
 
 // HELPER FUNCTIONS
 const {
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -5,6 +5,24 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const User = require("../models/User.model");
 
+//require uploader, already exported from cloudinary-setup.js
+const uploader = require("./../configs/cloudinary-setup");
+
+// include CLOUDINARY:
+//upload a single image per once.
+// ADD an horitzontal middleware
+router.post("/upload", uploader.single("image"), (req, res, next) => {
+  console.log("file is: ", req.file);
+
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // get secure_url from the file object and save it in the
+  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+  res.json({ secure_url: req.file.secure_url });
+});
+
 // HELPER FUNCTIONS
 const {
   isLoggedIn,
