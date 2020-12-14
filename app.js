352eb62e8f8ace44554638b855d348c9738d9230
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-12-14 18:55:01 +0100

    Initial deployment

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN],
+    origin: [
+      process.env.PUBLIC_DOMAIN,
+      'http://travel-guru.herokuapp.com',
+      'https://travel-guru.herokuapp.com'
+    ],
   }),
 );
 
@@ -65,7 +69,11 @@ app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
 
-
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no previous routes match the request, send back the React app.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // ERROR HANDLING
 //  Catch 404 and respond with error message
