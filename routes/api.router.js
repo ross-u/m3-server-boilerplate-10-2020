bb0ab519624f333af99f738d6be7f18de86b2a6b
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-12-15 13:16:40 +0100

    change model names to lowercase.

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
-const User = require("../models/User.model");
-const Post = require("../models/Post.model");
-const Comment = require("../models/Comment.model");
+const User = require("../models/user.model");
+const Post = require("../models/post.model");
+const Comment = require("../models/comment.model");
 const TravelLog = require("../models/TravelLog.model");
 
 //require uploader, already exported from cloudinary-setup.js
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 
 //require uploader, already exported from cloudinary-setup.js
 const uploader = require("./../config/cloudinary-setup");
